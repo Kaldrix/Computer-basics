7d4e083aeb7ac49286e5bc4df842f796aa963be8
Kaldrix <nhurox@gmail.com>
2024-10-15 10:24:48 +0200

    Update Kineticenergy.py

--- Kineticenergy.py
+++ Kineticenergy.py
@@ -1,9 +1,10 @@
+#Code to introduce the value of the mass
 mass=float (input ("Enter the mass in kg"))
 int(mass)
-
+#Code to introduce the value of the speed/velocity
 speed=float (input("Enter the speed in m/s"))
 int(speed)
-
+#Calculating the kinetic energy
 Kinetic_energy= 0.5*mass * speed**2
 print (f"The kietic energy is equal to {Kinetic_energy} Jules")
 
