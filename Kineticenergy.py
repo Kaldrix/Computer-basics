ef67a1687d9001092d7d928b0236c34f29258b90
Kaldrix <nhurox@gmail.com>
2024-10-15 10:23:19 +0200

    Add files via upload

---
+++ Kineticenergy.py
@@ -0,0 +1,9 @@
+mass=float (input ("Enter the mass in kg"))
+int(mass)
+
+speed=float (input("Enter the speed in m/s"))
+int(speed)
+
+Kinetic_energy= 0.5*mass * speed**2
+print (f"The kietic energy is equal to {Kinetic_energy} Jules")
+
---
+++ temperatureconversion.py
@@ -0,0 +1,7 @@
+Celsius_grades= input ("Introduce a value in degrees")
+int(Celsius_grades)
+
+
+Farenheit_grades =(int (Celsius_grades)* 1.8 + 32)
+print ("The temperature in Farenheits is", Farenheit_grades)
+
