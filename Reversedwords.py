0ed8713cfa44057e3308bc332624e92906119a39
Kaldrix <nhurox@gmail.com>
2024-10-16 20:05:58 +0200

    Add files via upload

---
+++ Reversedwords.py
@@ -0,0 +1,7 @@
+#Program to intoduce the code
+sentence= input ("Enter a sentence")
+str (sentence)
+
+#Reversing the sentence
+Reversed_sentence= "". join (sentence.split () [::-1]) 
+print (Reversed_sentence)
