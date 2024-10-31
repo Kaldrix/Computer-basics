4d36164373ea3adba69b6abc385484ef6f77bee4
Kaldrix <nhurox@gmail.com>
2024-10-31 18:44:05 +0100

    Delete temperatureconversion.py

--- temperatureconversion.py
+++
@@ -1,8 +0,0 @@
-#This code asks for the value of the temperature in Celsius
-Celsius_grades= input ("Introduce a value in degrees")
-int(Celsius_grades)
-
-#This code converts the Celsius grades into Farenheit grades
-Farenheit_grades =(int (Celsius_grades)* 1.8 + 32)
-print ("The temperature in Farenheits is", Farenheit_grades)
-
